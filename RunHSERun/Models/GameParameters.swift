64e8a1d437d20d7f1b74dc905d040da5a224a1e5
xfiniks <75494170+xfiniks@users.noreply.github.com>
2022-05-20 05:09:48 +0300

    Add files via upload

---
+++ RunHSERun/Models/GameParameters.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+class GameParameters {
+    
+    static let game = GameParameters()
+    
+    var audience : Int?
+    
+    var opponent : Int?
+    
+    var opponentsName : String?
+    
+    var audiences : Rooms?
+    
+    var gameID : Int?
+    
+    var gameTime : Int?
+    
+    var gameResult : String?
+    
+}
